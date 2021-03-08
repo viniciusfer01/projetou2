3978d45bdeed6aa5630a7f953f09c86eee8baf90
Vinícius Fernandes <business.fernandes22@gmail.com>
2021-03-07 23:27:02 -0300

    implemented put function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+#include <iomanip> 
+
+using namespace std;
 
 struct Voxel {
   float r,g,b; // Colors
@@ -27,14 +33,104 @@ public:
 };
 
 int main() {
-    Sculptor Voxel(3,2,4);
-    char cu = 'c';
-    char* cuu = &cu;
-
+    Sculptor Voxel(5,5,5);
     Voxel.setColor(1,0,0,1);
     Voxel.putVoxel(1,1,1);
 
+    // *char cu = 'c';
+    // char* cuu = &cu;
     Voxel.writeOFF(cuu);
 
     return 0;
 }
+
+void Sculptor::cutVoxel(int x, int y, int z){
+  v[x0][y0][z0].isOn = false;
+}
+
+void Sculptor::putVoxel(int x, int y, int z){
+  v[x0][y0][z0].isOn = true;
+  v[x0][y0][z0].r = r;
+  v[x0][y0][z0].g = g;
+  v[x0][y0][z0].b = b;
+  v[x0][y0][z0].a = a;
+
+}
+
+void Sculptor::writeOFF(char* filename){
+  int total, index;
+  ofstream f;
+  total = 0;
+  f.open(filename);
+  f << "OFF\n";
+
+  for (x = 0; x < nx; x++)
+  {
+    for (y = 0; y < ny; y++)
+    {
+      for (z = 0; z < nz; z++)
+      {
+        if (v[x][y][z].isOn) {
+          total++;
+        }
+      }
+      
+    }
+    
+  }
+
+  f << total * 8 << " " << total * 6 << "0 \n";
+
+  for (x = 0; x < nx; x++)
+  {
+    for (y = 0; y < ny; y++)
+    {
+      for (z = 0; z < nz; z++)
+      {
+        if (v[x][y][z].isOn) {
+          f << x - lado << " " << y + lado << " " << z - lado << "\n" << flush;
+          f << x - lado << " " << y - lado << " " << z - lado << "\n" << flush;
+          f << x + lado << " " << y - lado << " " << z - lado << "\n" << flush;
+          f << x + lado << " " << y + lado << " " << z - lado << "\n" << flush;
+          f << x - lado << " " << y + lado << " " << z + lado << "\n" << flush;
+          f << x - lado << " " << y - lado << " " << z + lado << "\n" << flush;
+          f << x + lado << " " << y - lado << " " << z + lado << "\n" << flush;
+          f << x + lado << " " << y + lado << " " << z + lado << "\n" << flush;
+  
+        }
+      }
+    }
+      
+  }
+
+  total = 0;  
+  for (x = 0; x < nx; x++)
+  {
+    for (y = 0; y < ny; y++)
+    {
+      for (z = 0; z < nz; z++)
+      {
+        if (v[x][y][z].isOn) {
+          index = total * 8;
+
+          f << std::fixed;
+          f << 4 << " " << index + 0 << " " << index + 3 << " " << index + 2 << " " index + 1 << " ";
+          f << std::setprecision(2) << v[x][y][z].r << " "
+            << std::setprecision(2) << v[x][y][z].g << " "
+            << std::setprecision(2) << v[x][y][z].b << " "
+            << std::setprecision(2) << v[x][y][z].a << "\n";
+
+          f << 4 << " " << index + 4 << " " << index + 5 << " " << index + 6 << " " index + 7 << " ";
+          f << std::setprecision(2) << v[x][y][z].r << " "
+            << std::setprecision(2) << v[x][y][z].g << " "
+            << std::setprecision(2) << v[x][y][z].b << " "
+            << std::setprecision(2) << v[x][y][z].a << "\n";
+        }
+      }
+      
+    }
+    
+  }
+}
+
+  
